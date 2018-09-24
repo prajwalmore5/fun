02629592b526ff3881e75c424e3bfe6358adc109
prajwalmore5 <39994719+prajwalmore5@users.noreply.github.com>
2018-09-24 22:27:04 +0530

    Add files via upload

---
+++ whatsapp spamming.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome('E:\Work\Praxis posters\chromedriver.exe')
+driver.get('https://web.whatsapp.com/')
+
+name = input('enter name')
+msg = input('enter message')
+count = int(input('enter count'))
+
+input('Enter anything after scanning QR code')
+
+driver.find_element_by_class_name('_2MSJr').send_keys(name)
+driver.find_element_by_class_name('_2MSJr').send_keys(Keys.ENTER)
+
+
+msg_box = driver.find_element_by_class_name('_2S1VP')
+for i in range(count):
+    msg_box.send_keys(msg)
+    driver.find_element_by_class_name('_35EW6')
+    driver.find_element_by_class_name('_35EW6').send_keys(Keys.ENTER)
